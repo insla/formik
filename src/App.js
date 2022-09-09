ebeb591a4656baeb158830a1d0c2f80e8248834f
Slava <shpirkin95@mail.ru>
2022-09-09 11:08:43 +0300

    formik

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './app.scss';
+import FormUpdate from './Form';
+
+function App() {
+  return (
+    <div className="app">
+        <FormUpdate/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FormUpdate.js
@@ -0,0 +1,101 @@
+import { Formik, Field, Form, ErrorMessage, useField } from 'formik';
+import * as Yup from 'yup';
+
+const MyTextInput = ({label, ...props}) => {
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.name}>{label}</label>
+            <input {...props} {...field}/>
+            {meta.touched && meta.error ? (
+                <div className='error'>{meta.error}</div>
+            ) : null}
+
+        </>
+    )
+}
+
+const FormUpdate = () => {
+
+    return (
+        <Formik
+            initialValues = {{
+                name: '',
+                email: '',
+                amount: 0,
+                currency: '',
+                text: '',
+                terms: false
+            }}
+            validationSchema = {Yup.object({
+                name: Yup.string()
+                        .min(2, 'Минимум 2 символа для заполнения!')
+                        .required('Обязательное поле!'),
+                email: Yup.string()
+                        .email('Неправильный email адрес!')
+                        .required('Обязательное поле!'),
+                amount: Yup.number()
+                        .min(5, 'Не мение пяти!')
+                        .required('Обязательное поле!'),
+                currency: Yup.string().required('Выберите валюту!'),
+                text: Yup.string()
+                        .min(10, 'Минимум 10 символа для заполнения!'),
+                terms: Yup.boolean()
+                        .required('Обязательное поле!')
+                        .oneOf([true], 'Обязательное поле!')
+            })}
+            onSubmit = {values => console.log(JSON.stringify(values, null, 2))}
+
+            >
+            <Form className="form">
+                <h2>Отправить пожертвование</h2>
+                <MyTextInput 
+                    label = "Ваше имя"
+                    id="name"
+                    name="name"
+                    type="text"/>
+                <MyTextInput 
+                    label = "Ваша почта"
+                    id="name"
+                    name="name"
+                    type="text"/>
+                <label htmlFor="amount">Количество</label>
+                <Field
+                    id="amount"
+                    name="amount"
+                    type="number"
+                />
+                <ErrorMessage className='error' name = 'amount' component = 'div'/>
+                <label htmlFor="currency">Валюта</label>
+                <Field
+                    id="currency"
+                    name="currency"
+                    as='select'>
+                        <option value="">Выберите валюту</option>
+                        <option value="USD">USD</option>
+                        <option value="UAH">UAH</option>
+                        <option value="RUB">RUB</option>
+                </Field>
+                <ErrorMessage className='error' name = 'currency' component = 'div'/>
+                <label htmlFor="text">Ваше сообщение</label>
+                <Field 
+                    id="text"
+                    name="text"
+                    as='textarea'
+                />
+                <ErrorMessage className='error' name = 'text' component = 'div'/>
+                <label className="checkbox">
+                    <Field 
+                        name="terms" 
+                        type="checkbox"
+                    />
+                    Соглашаетесь с политикой конфиденциальности?
+                </label>
+                <ErrorMessage className='error' name = 'terms' component = 'div'/>
+                <button type="submit">Отправить</button>
+            </Form>
+        </Formik>
+    )
+}
+
+export default FormUpdate;
